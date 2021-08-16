d0316dbb99378e5f17e408b7045222828c516fde
TanusreeBasu <71438615+TanusreeBasu@users.noreply.github.com>
2021-08-16 17:52:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var ball;
+
+function setup() {
+  createCanvas(400, 400);
+
+  // ball = createSprite(200,200,20,20);
+  // ball.shapeColor= "red";
+}
+
+function draw() {
+  background(220);
+
+
+drawSprites();
+}
